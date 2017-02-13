f5e84d545347feb434612b8c3475fa31c72cb716
Danny Arvizu <darvizu061@gmail.com>
2017-02-13 15:32:59 -0600

    Added jquery lib and on hover effect on portfolio thumbs

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,19 @@
+//project thumbs
+var thumbs = $(".thumb");
+var portfolio = $("#portfolioContainer");
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
 }
 function closeNav() {
     document.getElementById("myNav").style.width = "0%";
 }
+
+$( document ).ready(function(){
+  thumbs.mouseenter(function(){
+    thumbs.css("opacity","0.5");
+    $(this).css("opacity", "1");
+  });
+  portfolio.mouseleave(function(){
+    thumbs.css("opacity","1");
+  });
+});
